98a29025d59711fa85366b536f8a81bf5fb3ff50
adrianamackow <52165025+adrianamackow@users.noreply.github.com>
2020-07-08 19:00:08 +0200

    Update open_np.py

--- open_np.py
+++ open_np.py
@@ -5,7 +5,9 @@ from scipy.sparse import csr_matrix
 from sklearn import datasets
 from pprint import pprint
 
+# Redukcja cech w rzadkich danych z wykorzystaniem funkcji TSVD
 
+# wczytanie danych
 data = np.load('data_3class_bagofwords.npy')
 features = data
 print(data.shape)
@@ -14,10 +16,10 @@ print(data.shape)
 
 # digits = datasets.load_digits()
 features = StandardScaler().fit_transform(data)
-# features = np.load("patryk.npy")
 features_sparse = csr_matrix(features)
+
+# zdefiniowanie operacji TSVD
 tsvd = TruncatedSVD(n_components=4873)
-# tsvd.fit(features)
 features_tsvd = tsvd.fit(features)
 tsvd_var_ratios = tsvd.explained_variance_ratio_
 
@@ -42,29 +44,30 @@ print("po:", features_sparse_tsvd.shape[1])
 np.save('data_3class_bag_of_words_tsvd.npy', features_sparse_tsvd)
 np.savetxt('data_3class_bag_of_words_tscd.csv', features_sparse_tsvd, delimiter=',')
 
-#############
-# data = np.load('data_bag_of.npy')
-# features = data
-# print(data.shape)
-#
-#
-# features = StandardScaler().fit_transform(data)
-# features_sparse = csr_matrix(features)
-# tsvd = TruncatedSVD(n_components=features_sparse.shape[1]-1)
-# features_tsvd = tsvd.fit(features)
-# tsvd_var_ratios = tsvd.explained_variance_ratio_
-#
-#
-# def select_n_components(var_ratio, goal_var):
-#     total_variance = 0.0
-#     n_components = 0
-#     for explained_variance in var_ratio:
-#         total_variance += explained_variance
-#         n_components += 1
-#         if total_variance >= goal_var:
-#             break
-#     return n_components
-#
-#
-# n_components = select_n_components(tsvd_var_ratios, 0.95)
-# print(n_components)
+# Zdefiniowanie i przeprowadzenie operacji TSVD z wartością o jeden mniejszą niż liczba cech
+'''
+data = np.load('data_bag_of.npy')
+features = data
+print(data.shape)
+
+
+features = StandardScaler().fit_transform(data)
+features_sparse = csr_matrix(features)
+tsvd = TruncatedSVD(n_components=features_sparse.shape[1]-1)
+features_tsvd = tsvd.fit(features)
+tsvd_var_ratios = tsvd.explained_variance_ratio_
+
+
+def select_n_components(var_ratio, goal_var):
+    total_variance = 0.0
+    n_components = 0
+    for explained_variance in var_ratio:
+        total_variance += explained_variance
+        n_components += 1
+        if total_variance >= goal_var:
+            break
+    return n_components
+
+
+n_components = select_n_components(tsvd_var_ratios, 0.95)
+print(n_components)'''
